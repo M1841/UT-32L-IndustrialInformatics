d8f1e75dac0f13ca1195810d3495afb1d409c271
Mihai Mureșan <mihaimuresan592003@gmail.com>
2025-03-06 16:15:22 +0200

    solve Lab01 Ex05 (arithmetic and geometric average

---
+++ Lab01/Ex05/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex05;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Enter a sequence of numbers, separated by spaces:");
+    string input = Console.ReadLine()!;
+    int[] nums = input.Split(' ').Select(int.Parse).ToArray();
+
+    double arithmeticAvg = 0;
+    double geometricAvg = 1;
+
+    foreach (int num in nums)
+    {
+      arithmeticAvg += num;
+      geometricAvg *= num;
+    }
+
+    arithmeticAvg /= nums.Length;
+    geometricAvg = double.RootN(geometricAvg, nums.Length);
+
+    Console.WriteLine($"Arithmetic average: {arithmeticAvg:0.##}");
+    Console.WriteLine($"Geometric average: {geometricAvg:0.##}");
+  }
+}
