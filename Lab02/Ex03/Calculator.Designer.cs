4610f997d4e316b7c2c7dfa84c6e2bbb280938c0
Mihai Mureșan <mihaimuresan592003@gmail.com>
2025-03-13 09:08:27 +0200

    move Lab02 Ex03 custom init logic out of InitializeComponent

--- Lab02/Ex03/Calculator.Designer.cs
+++ Lab02/Ex03/Calculator.Designer.cs
@@ -28,54 +28,8 @@
     {
       this.components = new System.ComponentModel.Container();
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      BackColor = Color.FromArgb(26, 26, 26);
       this.ClientSize = new System.Drawing.Size(530, 70);
-      Controls.Add(numBox1);
-      Controls.Add(numBox2);
-      Controls.Add(operatorMenu);
-      Controls.Add(equalLabel);
-      Controls.Add(resultBox);
       this.Text = "Calculator";
     }
-
-    private TextBox numBox1 = new()
-    {
-      BackColor = Color.FromArgb(42, 42, 42),
-      ForeColor = Color.FromArgb(239, 239, 239),
-      BorderStyle = BorderStyle.FixedSingle,
-      Location = new Point(20, 20)
-    };
-    private TextBox numBox2 = new()
-    {
-      BackColor = Color.FromArgb(42, 42, 42),
-      ForeColor = Color.FromArgb(239, 239, 239),
-      BorderStyle = BorderStyle.FixedSingle,
-      Location = new Point(270, 20)
-    };
-
-    private MenuStrip operatorMenu = new()
-    {
-      BackColor = Color.FromArgb(42, 42, 42),
-      ForeColor = Color.FromArgb(239, 239, 239),
-      Location = new Point(130, 20),
-      Dock = DockStyle.None
-    };
-
-    private Label equalLabel = new()
-    {
-      Text = "=",
-      AutoSize = true,
-      ForeColor = Color.FromArgb(239, 239, 239),
-      Location = new Point(380, 20)
-    };
-
-    private TextBox resultBox = new()
-    {
-      BackColor = Color.FromArgb(42, 42, 42),
-      ForeColor = Color.FromArgb(239, 239, 239),
-      BorderStyle = BorderStyle.FixedSingle,
-      Location = new Point(410, 20),
-      ReadOnly = true
-    };
   }
 }
--- Lab02/Ex03/Calculator.cs
+++ Lab02/Ex03/Calculator.cs
@@ -4,6 +4,14 @@ namespace Ex03
   {
     public Calculator()
     {
+      BackColor = Color.FromArgb(26, 26, 26);
+
+      Controls.Add(numBox1);
+      Controls.Add(numBox2);
+      Controls.Add(operatorMenu);
+      Controls.Add(equalLabel);
+      Controls.Add(resultBox);
+
       foreach (string op in new[] { "+", "-", "*", "/" })
       {
         ToolStripMenuItem item = new()
@@ -15,6 +23,46 @@ namespace Ex03
       }
       InitializeComponent();
     }
+
+    private readonly TextBox numBox1 = new()
+    {
+      BackColor = Color.FromArgb(42, 42, 42),
+      ForeColor = Color.FromArgb(239, 239, 239),
+      BorderStyle = BorderStyle.FixedSingle,
+      Location = new Point(20, 20)
+    };
+    private readonly TextBox numBox2 = new()
+    {
+      BackColor = Color.FromArgb(42, 42, 42),
+      ForeColor = Color.FromArgb(239, 239, 239),
+      BorderStyle = BorderStyle.FixedSingle,
+      Location = new Point(270, 20)
+    };
+
+    private readonly MenuStrip operatorMenu = new()
+    {
+      BackColor = Color.FromArgb(42, 42, 42),
+      ForeColor = Color.FromArgb(239, 239, 239),
+      Location = new Point(130, 20),
+      Dock = DockStyle.None
+    };
+
+    private readonly Label equalLabel = new()
+    {
+      Text = "=",
+      AutoSize = true,
+      ForeColor = Color.FromArgb(239, 239, 239),
+      Location = new Point(380, 20)
+    };
+
+    private readonly TextBox resultBox = new()
+    {
+      BackColor = Color.FromArgb(42, 42, 42),
+      ForeColor = Color.FromArgb(239, 239, 239),
+      BorderStyle = BorderStyle.FixedSingle,
+      Location = new Point(410, 20),
+      ReadOnly = true
+    };
 
     private void Compute(string op)
     {
